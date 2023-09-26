f758880edc107bbc3e486dc791f244b01dba165f
fozboom <fozboom@gmail.com>
2023-09-26 13:43:07 +0300

    lab2 part 3)))

---
+++ myLabs/labNumber2 : Mystring/class.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Даниель Гнетецкий on 26.09.23.
+//
+
+#include "class.h"
---
+++ myLabs/labNumber2 : Mystring/class.h
@@ -0,0 +1,15 @@
+//
+// Created by Даниель Гнетецкий on 26.09.23.
+//
+
+#ifndef UNTITLED_CLASS_H
+#define UNTITLED_CLASS_H
+
+
+class class
+{
+
+};
+
+
+#endif //UNTITLED_CLASS_H
